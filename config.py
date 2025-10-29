e9f8767c43c8060025772fabf405916e5a6eff85
lavien12345 <cookie.lu66@gmail.com>
2025-10-30 01:59:51 +0800

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-DB_USER ='root'
-DB_PASSWORD = '20050823'
-DB_HOST = 'localhost'
-DB_NAME = 'my_app'
-
-SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
